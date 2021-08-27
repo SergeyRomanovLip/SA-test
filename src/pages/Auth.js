a7c8d28c245ca64d3459dac95994530c11050619
Sergey <nashe.dobrov@gmail.com>
2021-08-27 09:22:16 +0300

    v1.27

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,10 +1,9 @@
 import { useContext, useEffect, useState } from 'react'
-import { Button, Form, Container, Header, Dimmer, Loader, Label, Input } from 'semantic-ui-react'
+import { Button, Form, Container, Header, Dimmer, Loader, Label, Input, Popup } from 'semantic-ui-react'
 import { getData } from '../backend/firebase'
 import SearchName from '../components/SearchName'
 import { RoutesContext } from '../context/RoutesContext'
 
-//Добавить в firebase таблицу: компания -название, урлы - для доступа к вопросам и списку сотрудников, коды - для проверки принадлежности
 export const Auth = () => {
   const { histories } = useContext(RoutesContext)
   const [formState, setFormState] = useState({
@@ -12,7 +11,7 @@ export const Auth = () => {
     position: '',
     department: '',
     password: '',
-    company: ''
+    company: '',
   })
   const [empPassword, setEmpPassword] = useState()
   const [company, setCompany] = useState()
@@ -27,7 +26,7 @@ export const Auth = () => {
 
   const companyChangeHandler = async (tryCompany) => {
     setLoading(true)
-    let res = await getData('testEmployees', tryCompany)
+    let res = await getData('testEmployees', tryCompany.toUpperCase())
     if (res) {
       setCompany(tryCompany)
       setEmplList(res)
@@ -38,7 +37,7 @@ export const Auth = () => {
         position: '',
         department: '',
         company: '',
-        password: ''
+        password: '',
       })
     }
     setLoading(false)
@@ -52,7 +51,7 @@ export const Auth = () => {
     setFormState({
       fio: data.title,
       position: data.position,
-      department: data.department
+      department: data.department,
     })
     setEmpPassword(data.password)
   }
@@ -105,15 +104,31 @@ export const Auth = () => {
               {formState.department && formState.fio && formState.position ? (
                 <Form.Field>
                   <Label>Укажите ваш код</Label>
-                  <input name='password' value={formState.password} onChange={changeHandler} placeholder='Ваш код (3 последние цифры паспорта)' />
+                  <input
+                    name='password'
+                    value={formState.password}
+                    onChange={changeHandler}
+                    placeholder='Ваш код (3 последние цифры паспорта)'
+                  />
                 </Form.Field>
               ) : null}
               <Button
-                disabled={formState.department && formState.fio && formState.position && formState.password * 1 === empPassword * 1 ? false : true}
+                disabled={
+                  formState.department &&
+                  formState.fio &&
+                  formState.position &&
+                  formState.password * 1 === empPassword * 1
+                    ? false
+                    : true
+                }
                 color='blue'
                 type='submit'
                 onClick={() => {
-                  histories.push(`/testready/${company}&${formState.fio}&${formState.position}&${formState.department}/`)
+                  histories.push(
+                    `/testready/${company.toUpperCase()}&${formState.fio}&${formState.position}&${
+                      formState.department
+                    }/`
+                  )
                 }}
               >
                 Подтвердить
@@ -122,8 +137,8 @@ export const Auth = () => {
           ) : (
             <>
               <Form.Field>
-                <Label>Укажите название вашей компании</Label>
-                <Input name='company' value={formState.company} onChange={changeHandler} placeholder='Ваша компания' />
+                <Label>Трехзначный код вашей компании</Label>
+                <Input name='company' value={formState.company} onChange={changeHandler} placeholder='Например KMS' />
               </Form.Field>
               <Button
                 color='teal'
