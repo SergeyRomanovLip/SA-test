2b2f103a230d8b572d062bd6f2765bca4b80e65e
Sergey <nashe.dobrov@gmail.com>
2021-08-27 10:16:50 +0300

    v1.299

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Icon, Menu, Sticky } from 'semantic-ui-react'
+import { Icon, Menu, Sticky, Divider } from 'semantic-ui-react'
 import { logOut } from '../backend/firebase'
 import { AppContext } from '../context/AppContext'
 
 export const Toolbar = () => {
+  const { width } = useContext(AppContext)
   const hstr = useHistory()
   const [state, setState] = useState({ activeItem: hstr.location.pathname.split('/')[1] })
   const { authenticated } = useContext(AppContext)
@@ -16,21 +17,22 @@ export const Toolbar = () => {
   }, [hstr])
 
   return (
-    <Sticky>
-      <Menu tabular color='teal' style={{ backgroundColor: 'white' }}>
-        {authenticated && (
-          <Menu.Item
-            name='logout'
-            active={state.activeItem === 'logout'}
-            onClick={() => {
-              logOut()
-            }}
-          >
-            <Icon name='log out' />
-            Выйти
-          </Menu.Item>
-        )}
-        {/* <Menu.Item
+    <>
+      <Sticky>
+        <Menu tabular color='teal' style={{ backgroundColor: 'white' }}>
+          {authenticated && (
+            <Menu.Item
+              name='logout'
+              active={state.activeItem === 'logout'}
+              onClick={() => {
+                logOut()
+              }}
+            >
+              <Icon name='log out' />
+              Выйти
+            </Menu.Item>
+          )}
+          {/* <Menu.Item
           name='home'
           active={state.activeItem === 'home'}
           onClick={() => {
@@ -42,28 +44,45 @@ export const Toolbar = () => {
           На главную
         </Menu.Item> */}
 
-        <Menu.Item
-          name='auth'
-          active={state.activeItem === 'auth' || state.activeItem === 'testready'}
-          onClick={() => {
-            hstr.push('/auth')
-          }}
-        >
-          <Icon name='clipboard list' />
-          Тесты
-        </Menu.Item>
+          <Menu.Item
+            name='auth'
+            active={state.activeItem === 'auth' || state.activeItem === 'testready'}
+            onClick={() => {
+              hstr.push('/auth')
+            }}
+          >
+            <Icon name='clipboard list' />
+            Тесты
+          </Menu.Item>
 
-        <Menu.Item
-          name='admin'
-          active={state.activeItem === 'admin'}
-          onClick={() => {
-            hstr.push('/admin')
-          }}
-        >
-          <Icon name='adn' />
-          Панель администратора
-        </Menu.Item>
-      </Menu>
-    </Sticky>
+          <Menu.Item
+            name='admin'
+            active={state.activeItem === 'admin'}
+            onClick={() => {
+              hstr.push('/admin')
+            }}
+          >
+            <Icon name='adn' />
+            Панель администратора
+          </Menu.Item>
+          <Menu.Item position={'right'}>
+            {width > 900 && (
+              <>
+                <Icon color={'teal'} name={'info circle'}></Icon>
+                При возникновении проблем писать в WhatsApp &nbsp;<a href='tel:+7-962-351-80-86'>+7 (962) 351-80-86 </a>
+              </>
+            )}
+          </Menu.Item>
+        </Menu>
+      </Sticky>
+      {width < 901 && (
+        <Menu style={{ position: 'fixed', bottom: 0, zIndex: 999 }}>
+          <Menu.Item position={'center'}>
+            <Icon color={'teal'} name={'info circle'}></Icon>
+            При возникновении проблем писать в WhatsApp &nbsp;<a href='tel:+7-962-351-80-86'>+7 (962) 351-80-86 </a>
+          </Menu.Item>
+        </Menu>
+      )}
+    </>
   )
 }
